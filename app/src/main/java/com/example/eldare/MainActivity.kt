a6d389be5189b8b144ec517fcc2e960aed2634c8
VlaKarlo <116498296+VlaKarlo@users.noreply.github.com>
2025-01-19 22:57:49 +0100

    setup

--- app/src/main/java/com/example/eldare/MainActivity.kt
+++ app/src/main/java/com/example/eldare/MainActivity.kt
@@ -13,35 +13,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.eldare.ui.theme.EldareTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             EldareTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    EldareTheme {
-        Greeting("Android")
-    }
-}
